e3c1a320c0748ab390d635836ed1e6ab741e2194
julia <julsuniverse@gmail.com>
2021-12-24 17:33:45 +0200

    Extract action to controller

--- api/public/index.php
+++ api/public/index.php
@@ -3,15 +3,17 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use App\Http\Action;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
-});
+$action = function () {
+    return new Action\HomeAction();
+};
+
+$app->get('/', $action());
 
 $app->get('/test', function (Request $request, Response $response, $args) {
     $response->getBody()->write("<h1>This is a test page</h1>");
