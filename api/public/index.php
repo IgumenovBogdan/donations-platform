5f74b2da949a1b06711597f24346b16466568832
julia <julsuniverse@gmail.com>
2021-12-30 16:07:34 +0200

    Add middleware

--- api/public/index.php
+++ api/public/index.php
@@ -2,6 +2,9 @@
 
 use DI\ContainerBuilder;
 use Slim\Factory\AppFactory;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+
 
 chdir(dirname(__DIR__));
 require __DIR__ . '/../vendor/autoload.php';
@@ -13,6 +16,13 @@ $builder->addDefinitions($config);
 $container = $builder->build();
 
 $app = AppFactory::createFromContainer($container);
+
+$app->add(function(Request $request, RequestHandler $handler) {
+    $response = $handler->handle($request);
+    echo PHP_EOL . ('This is middleware!') . PHP_EOL;
+    return $response;
+});
+
 (require 'config/routes.php')($app);
 
 $app->run();
