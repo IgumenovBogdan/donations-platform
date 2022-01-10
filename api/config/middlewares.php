5d4a10cfa416e65a94ab7803711dbcd8949578c9
julia <julsuniverse@gmail.com>
2022-01-10 14:05:10 +0200

    Add exception handler

--- api/config/middlewares.php
+++ api/config/middlewares.php
@@ -2,9 +2,11 @@
 
 declare(strict_types=1);
 
+use App\Http\Middleware\ExceptionHandler;
 use App\Http\Middleware\TestMiddleware;
 use Slim\App;
 
 return function (App $app) {
     $app->add(TestMiddleware::class);
+    $app->add(ExceptionHandler::class);
 };
